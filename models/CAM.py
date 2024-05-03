9691318dc2bbc05b0b3e3d4b87f8fae056c07f4c
vghuy <120817424+vghuy@users.noreply.github.com>
2024-05-04 05:39:24 +0700

    Update CAM.py

--- models/CAM.py
+++ models/CAM.py
@@ -2,44 +2,36 @@ import math
 import torch
 import torch.nn as nn
 
-class CAMFunction(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, input):
-        ctx.save_for_backward(input)
-        avg_pool = nn.AdaptiveAvgPool2d(1)
-        max_pool = nn.AdaptiveMaxPool2d(1)
-        fc1 = nn.Conv2d(input.shape[1], input.shape[1] // 16, kernel_size=1, padding=0)
-        relu = nn.ReLU(inplace=True)
-        fc2 = nn.Conv2d(input.shape[1] // 16, input.shape[1], kernel_size=1, padding=0)
-        sigmoid_channel = nn.Sigmoid()
-
-        avg = avg_pool(input)
-        mx = max_pool(input)
-        avg = fc1(avg)
-        mx = fc1(mx)
-        avg = relu(avg)
-        mx = relu(mx)
-        avg = fc2(avg)
-        mx = fc2(mx)
-        x = avg + mx
-        x = sigmoid_channel(x)
-        x = input * x
-
-        return x
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        input, = ctx.saved_tensors
-        # Gradient computation not required for CAM, return None
-        return None
-
 class CAM(nn.Module):
+
     def __init__(self, channels, reduction):
         super(CAM, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc1 = nn.Conv2d(channels, channels // reduction, kernel_size=1,
+                             padding=0)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Conv2d(channels // reduction, channels, kernel_size=1,
+                             padding=0)
+        self.sigmoid_channel = nn.Sigmoid()
+        self.conv_after_concat = nn.Conv2d(2, 1, kernel_size = 3, stride=1, padding = 1)
+        self.sigmoid_spatial = nn.Sigmoid()
         self.ch=channels
 
     def forward(self, x):
         # Channel attention module
+        # print(x.shape)
+        # print(self.ch)
         module_input = x
-        x = CAMFunction.apply(x)
+        avg = self.avg_pool(x)
+        mx = self.max_pool(x)
+        avg = self.fc1(avg)
+        mx = self.fc1(mx)
+        avg = self.relu(avg)
+        mx = self.relu(mx)
+        avg = self.fc2(avg)
+        mx = self.fc2(mx)
+        x = avg + mx
+        x = self.sigmoid_channel(x)
+        x = module_input * x
         return x
